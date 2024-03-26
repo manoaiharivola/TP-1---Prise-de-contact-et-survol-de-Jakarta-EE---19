e3ce1ac6ed7c9cc72f55ca58b4621711697e8734
manoaiharivolaa <manoaiharivola@gmail.com>
2024-03-27 00:06:48 +0300

    15.d. Affichage des Discount dans la page des détails d'un client - Ajout d'un bean CDI pour gérer les données de la base sur les codes de réduction

---
+++ src/main/java/mg/manoa/iharivola/tpcustomermanoaiharivola19/service/DiscountManager.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.manoa.iharivola.tpcustomermanoaiharivola19.service;
+
+import mg.manoa.iharivola.tpcustomermanoaiharivola19.entity.Discount;
+import java.util.List;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+/**
+ * Façade pour gérer les Discounts.
+ *
+ * @author My PC
+ */
+@RequestScoped
+public class DiscountManager {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Discount> getAllDiscounts() {
+        Query query = em.createNamedQuery("Discount.findAll");
+        return query.getResultList();
+    }
+
+    public Discount findById(String code) {
+        return em.find(Discount.class, code);
+    }
+}
