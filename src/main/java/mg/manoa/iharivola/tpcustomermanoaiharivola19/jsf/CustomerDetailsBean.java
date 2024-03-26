89d528027395231229e056f7d9c8b8b499fcd12a
manoaiharivolaa <manoaiharivola@gmail.com>
2024-03-26 23:17:24 +0300

    15.b. Ajout d'un backing bean pour la page des détails

---
+++ src/main/java/mg/manoa/iharivola/tpcustomermanoaiharivola19/jsf/CustomerDetailsBean.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.manoa.iharivola.tpcustomermanoaiharivola19.jsf;
+
+import mg.manoa.iharivola.tpcustomermanoaiharivola19.entity.Customer;
+import mg.manoa.iharivola.tpcustomermanoaiharivola19.service.CustomerManager;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author My PC Backing bean pour la page customerDetails.xhtml.
+ */
+@Named(value = "customerDetailsBean")
+@ViewScoped
+public class CustomerDetailsBean implements Serializable {
+
+    private int idCustomer;
+    private Customer customer;
+
+    @Inject
+    private CustomerManager customerManager;
+
+    public int getIdCustomer() {
+        return idCustomer;
+    }
+
+    public void setIdCustomer(int idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    /**
+     * Retourne les détails du client courant (contenu dans l'attribut customer
+     * de cette classe).
+     * @return 
+     */
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    /**
+     * Action handler - met à jour dans la base de données les données du client
+     * contenu dans la variable d'instance customer.
+     *
+     * @return la prochaine page à afficher, celle qui affiche la liste des
+     * clients.
+     */
+    public String update() {
+        // Modifie la base de données.
+        // Il faut affecter à customer (sera expliqué dans le cours).
+        customer = customerManager.update(customer);
+        return "customerList";
+    }
+
+    public void loadCustomer() {
+        this.customer = customerManager.findById(idCustomer);
+    }
+}
--- src/main/java/mg/manoa/iharivola/tpcustomermanoaiharivola19/service/CustomerManager.java
+++ src/main/java/mg/manoa/iharivola/tpcustomermanoaiharivola19/service/CustomerManager.java
@@ -37,4 +37,8 @@ public class CustomerManager {
     public void persist(Customer customer) {
         em.persist(customer);
     }
+
+    public Customer findById(int idCustomer) {
+        return em.find(Customer.class, idCustomer);
+    }
 }
